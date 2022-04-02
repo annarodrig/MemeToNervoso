c475e3dd79abc694c01e2a45fc697a339a8e0c79
Anna Beztriz <annab_oliver@hotmail.com>
2022-04-01 22:12:32 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseUsuario, fraseMemeToNervoso;
+
+Console.WriteLine("Usuário, o que você pensa sobre mim?");
+fraseUsuario = Console.ReadLine()!;
+
+// Remove os últimos caracteres da frase digitada pelo usuario e troca pelo meme
+fraseMemeToNervoso = $"{fraseUsuario.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(fraseMemeToNervoso);
+
